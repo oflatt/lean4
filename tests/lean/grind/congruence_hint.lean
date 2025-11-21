069c7aa681feaacbede8ec480ae6f00161025a8c
oflatt <oflatt@gmail.com>
2025-11-21 13:05:49 -0800

    logging giving a correct hint

--- tests/lean/grind/congruence_hint.lean
+++ tests/lean/grind/congruence_hint.lean
@@ -1,11 +1,12 @@
 /--
-info: Hints
-hint 1 (congruence)
-  rules: (none)
-  conclusion:
-    f y = f z
-  premises:
-    y = z
+trace: [grind.hints] Hints
+  [grind.hints] hint 1 (congruence)
+    [grind.hints] rules
+      (none)
+    [grind.hints] conclusion
+        f y = f z
+    [grind.hints] premises
+        y = z
 -/
 #guard_msgs in
 example (f : Nat → Nat) (y z : Nat) (h : y = z) : f y = f z := by
