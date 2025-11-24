b366877c72a2d5336602632709a23dd02e68dc33
oflatt <oflatt@gmail.com>
2025-11-24 09:53:30 -0800

    smmal change

--- tests/lean/grind/congruence_hint.lean
+++ tests/lean/grind/congruence_hint.lean
@@ -1,13 +1,2 @@
-/--
-trace: [grind.hints] Hints
-  [grind.hints] hint 1 (congruence)
-    [grind.hints] rules
-      (none)
-    [grind.hints] conclusion
-      f y = f z
-    [grind.hints] premises
-      y = z
--/
-#guard_msgs in
 example (f : Nat → Nat) (y z : Nat) (h : y = z) : f y = f z := by
   grind??
